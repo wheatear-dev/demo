99c23f6b78ddbee4ab9cc11b9fa01cdd75fc15a7
Edward Moulsdale <eddie.moulsdale@gmail.com>
2024-12-28 20:05:21 +0000

    move around

---
+++ vite/src/components/utils/test.ts
@@ -0,0 +1,20 @@
+import { expect } from 'vitest'
+import { Hash } from '../types';
+
+
+function testHash(
+    hash: Hash,
+    maxLen: number = 20,
+    reps: number = 10
+) {
+    for (let i = 0; i < maxLen; i++) {
+        for (let j = 0; j < reps; j++ ) {
+            const in_ = Math.random().toString(36).slice(2, 2 + i);
+            const expected = hash.refFunc(in_);
+            const observed = hash.func(in_);
+            expect(expected === observed);
+        };
+    };
+};
+
+export default testHash;
